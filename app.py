d45f10c470b157eb7bd1511ffbfd9b601fd5adfd
caanyan91 <143204855+caanyan91@users.noreply.github.com>
2024-12-08 16:09:52 -0600

    Update app.py
    Updated port to work with AWS

--- app.py
+++ app.py
@@ -290,4 +290,4 @@ def download_file(job_id):
         return redirect(url_for('job_status_page', job_id=job_id))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
